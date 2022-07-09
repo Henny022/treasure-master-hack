6e4112a011c57896f3436beb201b2c4466a0729a
Henny022 <hen.geheim22@gmail.com>
2022-07-09 19:37:21 +0200

    add iterations as command line parameter to tests
    only few tests for now

--- bruteforce/tm_avx2_test/main.cpp
+++ bruteforce/tm_avx2_test/main.cpp
@@ -1,10 +1,16 @@
 #include "tm_avx2_test.h"
 #include "tester.h"
 
-int main()
+int main(int argc, char*argv[])
 {
+    if (argc <= 1)
+    {
+        printf("usage: test <iterations>\n");
+        exit(1);
+    }
 	tm_avx2_intrinsics_test tester;
 	run_validity_tests<tm_avx2_intrinsics_test>(tester);
 
-	run_speed_tests<tm_avx2_intrinsics_test>(tester, 10000000);
+    int n = std::strtol(argv[0], nullptr, 10);
+	run_speed_tests<tm_avx2_intrinsics_test>(tester, n);
 }
