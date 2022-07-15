d33e1a29fbcd3a4c95eba697de2fe963649d6947
Henny022 <hen.geheim22@gmail.com>
2022-07-16 00:49:48 +0200

    manage zmm loads and stores manually

--- bruteforce/tm_mpi/main.cpp
+++ bruteforce/tm_mpi/main.cpp
@@ -33,8 +33,12 @@ void run_once(const uint8_t IV[8],
         rng_seed = entry.rng_seed;
         uint16_t nibble_selector = entry.nibble_selector;
 
+        __m512i lo = working_code.zmm[0];
+        __m512i hi = working_code.zmm[1];
+
         for (int i = 0; i < 16; ++i)
         {
+            working_code.zmm[0] = lo;
             uint8_t current_byte = working_code.u8[i];
             if (nibble_selector & 0x8000)
             {
@@ -47,44 +51,30 @@ void run_once(const uint8_t IV[8],
             switch (algorithm)
             {
             case 0:
-                working_code.zmm[0] =
-                    _mm512_add_epi8(working_code.zmm[0], working_code.zmm[0]);
-                working_code.zmm[1] =
-                    _mm512_add_epi8(working_code.zmm[1], working_code.zmm[1]);
-                working_code.zmm[0] =
-                    working_code.zmm[0] | rng_tables.alg0[rng_seed][0];
-                working_code.zmm[1] =
-                    working_code.zmm[1] | rng_tables.alg0[rng_seed][1];
+                working_code.zmm[0] = _mm512_add_epi8(lo, lo);
+                hi = _mm512_add_epi8(hi, hi);
+                lo = lo | rng_tables.alg0[rng_seed][0];
+                hi = hi | rng_tables.alg0[rng_seed][1];
                 rng_seed = rng_tables.forward_128[rng_seed];
                 break;
             case 1:
-                working_code.zmm[0] = _mm512_add_epi8(
-                    working_code.zmm[0], rng_tables.regular[rng_seed][0]);
-                working_code.zmm[1] = _mm512_add_epi8(
-                    working_code.zmm[1], rng_tables.regular[rng_seed][1]);
+                lo = _mm512_add_epi8(lo, rng_tables.regular[rng_seed][0]);
+                hi = _mm512_add_epi8(hi, rng_tables.regular[rng_seed][1]);
                 rng_seed = rng_tables.forward_128[rng_seed];
                 break;
             case 2:
             {
                 __m512i even_lo =
-                    _mm512_srli_epi16(working_code.zmm[0] & mask_even, 1) &
-                    mask_even;
+                    _mm512_srli_epi16(lo & mask_even, 1) & mask_even;
                 __m512i even_hi =
-                    _mm512_srli_epi16(working_code.zmm[1] & mask_even, 1) &
-                    mask_even;
-                __m512i odd_lo =
-                    _mm512_slli_epi16(working_code.zmm[0] & mask_odd, 1) &
-                    mask_odd;
-                __m512i odd_hi =
-                    _mm512_slli_epi16(working_code.zmm[1] & mask_odd, 1) &
-                    mask_odd;
+                    _mm512_srli_epi16(hi & mask_even, 1) & mask_even;
+                __m512i odd_lo = _mm512_slli_epi16(lo & mask_odd, 1) & mask_odd;
+                __m512i odd_hi = _mm512_slli_epi16(hi & mask_odd, 1) & mask_odd;
 
-                __m512i carry_lo = _mm512_permutex2var_epi8(
-                    working_code.zmm[0], carry_permutation,
-                    working_code.zmm[1]);
+                __m512i carry_lo =
+                    _mm512_permutex2var_epi8(lo, carry_permutation, hi);
                 __m512i carry_hi = _mm512_permutex2var_epi8(
-                    working_code.zmm[1], carry_permutation,
-                    rng_tables.alg2[rng_seed]);
+                    hi, carry_permutation, rng_tables.alg2[rng_seed]);
 
                 even_lo = even_lo | (carry_lo & carry_mask_r & mask_even);
                 even_hi = even_hi | (carry_hi & carry_mask_r & mask_even);
@@ -92,47 +82,35 @@ void run_once(const uint8_t IV[8],
                 odd_lo = odd_lo | (carry_lo & carry_mask_l & mask_odd);
                 odd_hi = odd_hi | (carry_hi & carry_mask_l & mask_odd);
 
-                working_code.zmm[0] = even_lo | odd_lo;
-                working_code.zmm[1] = even_hi | odd_hi;
+                lo = even_lo | odd_lo;
+                hi = even_hi | odd_hi;
 
                 rng_seed = rng_tables.rng_table[rng_seed];
                 break;
             }
             case 3:
-                working_code.zmm[0] =
-                    working_code.zmm[0] ^ rng_tables.regular[rng_seed][0];
-                working_code.zmm[1] =
-                    working_code.zmm[1] ^ rng_tables.regular[rng_seed][1];
+                lo = lo ^ rng_tables.regular[rng_seed][0];
+                hi = hi ^ rng_tables.regular[rng_seed][1];
                 rng_seed = rng_tables.forward_128[rng_seed];
                 break;
             case 4:
-                working_code.zmm[0] = _mm512_sub_epi8(
-                    working_code.zmm[0], rng_tables.regular[rng_seed][0]);
-                working_code.zmm[1] = _mm512_sub_epi8(
-                    working_code.zmm[1], rng_tables.regular[rng_seed][1]);
+                lo = _mm512_sub_epi8(lo, rng_tables.regular[rng_seed][0]);
+                hi = _mm512_sub_epi8(hi, rng_tables.regular[rng_seed][1]);
                 rng_seed = rng_tables.forward_128[rng_seed];
                 break;
             case 5:
             {
                 __m512i even_lo =
-                    _mm512_slli_epi16(working_code.zmm[0] & mask_even, 1) &
-                    mask_even;
+                    _mm512_slli_epi16(lo & mask_even, 1) & mask_even;
                 __m512i even_hi =
-                    _mm512_slli_epi16(working_code.zmm[1] & mask_even, 1) &
-                    mask_even;
-                __m512i odd_lo =
-                    _mm512_srli_epi16(working_code.zmm[0] & mask_odd, 1) &
-                    mask_odd;
-                __m512i odd_hi =
-                    _mm512_srli_epi16(working_code.zmm[1] & mask_odd, 1) &
-                    mask_odd;
+                    _mm512_slli_epi16(hi & mask_even, 1) & mask_even;
+                __m512i odd_lo = _mm512_srli_epi16(lo & mask_odd, 1) & mask_odd;
+                __m512i odd_hi = _mm512_srli_epi16(hi & mask_odd, 1) & mask_odd;
 
-                __m512i carry_lo = _mm512_permutex2var_epi8(
-                    working_code.zmm[0], carry_permutation,
-                    working_code.zmm[1]);
+                __m512i carry_lo =
+                    _mm512_permutex2var_epi8(lo, carry_permutation, hi);
                 __m512i carry_hi = _mm512_permutex2var_epi8(
-                    working_code.zmm[1], carry_permutation,
-                    rng_tables.alg5[rng_seed]);
+                    hi, carry_permutation, rng_tables.alg5[rng_seed]);
 
                 even_lo = even_lo | (carry_lo & carry_mask_l & mask_even);
                 even_hi = even_hi | (carry_hi & carry_mask_l & mask_even);
@@ -140,30 +118,30 @@ void run_once(const uint8_t IV[8],
                 odd_lo = odd_lo | (carry_lo & carry_mask_r & mask_odd);
                 odd_hi = odd_hi | (carry_hi & carry_mask_r & mask_odd);
 
-                working_code.zmm[0] = even_lo | odd_lo;
-                working_code.zmm[1] = even_hi | odd_hi;
+                lo = even_lo | odd_lo;
+                hi = even_hi | odd_hi;
                 rng_seed = rng_tables.rng_table[rng_seed];
                 break;
             }
             case 6:
-                working_code.zmm[0] = _mm512_srli_epi16(working_code.zmm[0], 1);
-                working_code.zmm[0] = working_code.zmm[0] & srl_mask;
-                working_code.zmm[1] = _mm512_srli_epi16(working_code.zmm[1], 1);
-                working_code.zmm[1] = working_code.zmm[1] & srl_mask;
-                working_code.zmm[0] =
-                    working_code.zmm[0] | rng_tables.alg6[rng_seed][0];
-                working_code.zmm[1] =
-                    working_code.zmm[1] | rng_tables.alg6[rng_seed][1];
+                lo = _mm512_srli_epi16(lo, 1);
+                lo = lo & srl_mask;
+                hi = _mm512_srli_epi16(hi, 1);
+                hi = hi & srl_mask;
+                lo = lo | rng_tables.alg6[rng_seed][0];
+                hi = hi | rng_tables.alg6[rng_seed][1];
                 rng_seed = rng_tables.forward_128[rng_seed];
                 break;
             case 7:
-                working_code.zmm[0] = ~working_code.zmm[0];
-                working_code.zmm[1] = ~working_code.zmm[1];
+                lo = ~lo;
+                hi = ~hi;
                 break;
             default:
                 break;
             }
         }
+        working_code.zmm[0] = lo;
+        working_code.zmm[1] = hi;
     }
     // checksum result vector
     uint16_t sum = 0;
